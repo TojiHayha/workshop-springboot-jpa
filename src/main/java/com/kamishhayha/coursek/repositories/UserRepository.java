634e822bd444e93b4ec51048057768a84884e4da
TojiHayha <fernando.aq502@gmail.com>
2024-07-28 02:01:49 -0300

    JPA repository, dependecy injection, database seeding

---
+++ src/main/java/com/kamishhayha/coursek/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kamishhayha.coursek.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kamishhayha.coursek.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
