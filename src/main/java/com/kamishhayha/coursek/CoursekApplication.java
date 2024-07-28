f2aac8a0818f80169dc03c5d4fb78449e9dc5f72
TojiHayha <fernando.aq502@gmail.com>
2024-07-27 22:08:25 -0300

    Project created

---
+++ src/main/java/com/kamishhayha/coursek/CoursekApplication.java
@@ -0,0 +1,13 @@
+package com.kamishhayha.coursek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamishhayha/coursek/CoursekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamishhayha.coursek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
