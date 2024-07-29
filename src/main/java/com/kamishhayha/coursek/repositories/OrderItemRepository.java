d09ad11cbddd26809c15fd71efa0d9a2fc3424ed
TojiHayha <fernando.aq502@gmail.com>
2024-07-28 23:34:51 -0300

    Adjustment OrderItem Repository

--- src/main/java/com/kamishhayha/coursek/repositories/OrderItemRepository.java
+++ src/main/java/com/kamishhayha/coursek/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.kamishhayha.coursek.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kamishhayha.coursek.entities.OrderItem;
+import com.kamishhayha.coursek.entities.pk.OrderItemPK;
 
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
